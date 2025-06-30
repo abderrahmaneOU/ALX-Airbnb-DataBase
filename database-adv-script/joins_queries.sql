df899ebdcae99091a081d00057c28e75f1f9452f
Abderrahmane Oudi <abdoze56@outlook.com>
2025-06-30 17:55:16 +0100

    Add complex join queries for bookings, properties, and users

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,33 +1,37 @@
 -- INNER JOIN: Retrieve all bookings and the respective users who made those bookings
-SELECT
+SELECT 
     bookings.booking_id,
-    bookings.property_id,
     bookings.start_date,
     bookings.end_date,
     users.user_id,
     users.first_name,
-    users.last_name,
-    users.email
-FROM bookings
-INNER JOIN users ON bookings.user_id = users.user_id;
+    users.last_name
+FROM 
+    bookings
+INNER JOIN 
+    users ON bookings.user_id = users.user_id;
 
 -- LEFT JOIN: Retrieve all properties and their reviews, including properties that have no reviews
-SELECT
+SELECT 
     properties.property_id,
-    properties.title,
+    properties.name AS property_name,
     reviews.review_id,
     reviews.rating,
     reviews.comment
-FROM properties
-LEFT JOIN reviews ON properties.property_id = reviews.property_id;
+FROM 
+    properties
+LEFT JOIN 
+    reviews ON properties.property_id = reviews.property_id;
 
 -- FULL OUTER JOIN: Retrieve all users and all bookings, even if the user has no booking or a booking is not linked to a user
-SELECT
+SELECT 
     users.user_id,
     users.first_name,
     users.last_name,
     bookings.booking_id,
     bookings.start_date,
     bookings.end_date
-FROM users
-FULL OUTER JOIN bookings ON users.user_id = bookings.user_id;
+FROM 
+    users
+FULL OUTER JOIN 
+    bookings ON users.user_id = bookings.user_id;
