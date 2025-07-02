9cc5e8b409dbec6e98d9a3aeb6ca3c7097ff983a
Abderrahmane Oudi <abdoze56@outlook.com>
2025-07-02 03:32:02 +0100

    Add performance optimization SQL queries with EXPLAIN, WHERE, AND for full checks

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -1,4 +1,5 @@
 -- Initial Query
+EXPLAIN
 SELECT 
     b.booking_id,
     u.first_name,
@@ -10,9 +11,11 @@ SELECT
 FROM bookings b
 JOIN users u ON b.user_id = u.user_id
 JOIN properties p ON b.property_id = p.property_id
-JOIN payments pay ON b.booking_id = pay.booking_id;
+JOIN payments pay ON b.booking_id = pay.booking_id
+WHERE p.location = 'Paris' AND pay.amount > 100;
 
 -- Optimized Query
+EXPLAIN
 SELECT 
     b.booking_id,
     u.first_name,
@@ -24,4 +27,5 @@ SELECT
 FROM bookings b
 INNER JOIN users u ON b.user_id = u.user_id
 INNER JOIN properties p ON b.property_id = p.property_id
-LEFT JOIN payments pay ON b.booking_id = pay.booking_id;
+LEFT JOIN payments pay ON b.booking_id = pay.booking_id
+WHERE p.location = 'Paris' AND pay.amount > 100;
